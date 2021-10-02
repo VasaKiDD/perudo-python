3e16ba08e0ac6d727519aea97a245a40735ef53d
David Kas <david.kas@quant.global>
2021-10-02 12:14:11 +0200

    first commit

---
+++ perudo.py
@@ -0,0 +1,22 @@
+import numpy as np
+import time
+
+if __name__ == "__main__":
+    print("-----Welcome to the Perudo Game-----")
+
+    players = 0
+    while players > 5 or players < 1:
+        players = input("How many AI player (Accepted answer: 1-5) ?\n")
+        print("Player number must be between 1-5")
+
+    dices = {}
+    for i in range(players):
+        dices[i] = 5
+
+    print("The game is starting.\n")
+    print("Coosing randomly starting player.\n")
+    starting_player = np.random.randint(players)
+    if starting_player > 0:
+        print("Player {s} is starting")
+    else:
+        print("You start")
---
+++ src/ai_player.py
@@ -0,0 +1,3 @@
+class AIPlayer:
+    def __init__(self):
+        pass
